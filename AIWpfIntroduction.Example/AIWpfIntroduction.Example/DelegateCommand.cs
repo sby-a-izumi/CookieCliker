ce2bd6866e44fb33987048b54613a218f3ac6641
Akira Izumi <a-izumi@shibuya.co.jp>
2024-04-03 09:42:24 +0900

    DelegateCommandのUsing整理、コメント記述

--- AIWpfIntroduction.Example/AIWpfIntroduction.Example/DelegateCommand.cs
+++ AIWpfIntroduction.Example/AIWpfIntroduction.Example/DelegateCommand.cs
@@ -1,26 +1,36 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Input;
 
 namespace AIWpfIntroduction.Example
 {
+    /// <summary>
+    /// Viewの処理をViewModelへ伝えるためにICommandインターフェースを利用する必要があるので、デリゲートを用いて処理の委譲をするクラスです。
+    /// </summary>
     internal class DelegateCommand : ICommand
     {   
-        //コマンド実行時の処理内容を保持
+        /// <summary>
+        /// コマンド実行時の処理内容を保持
+        /// </summary>
         private Action<object> _execute;
 
-        //コマンド実行可能判別の処理内容を保持
+        /// <summary>
+        /// コマンド実行可能判別の処理内容を保持
+        /// </summary>
         private Func<object, bool> _canExecute;
 
-        //新しいインスタンスを生成（引数：execute→コマンド実行処理を指定　のコンストラクタ）
+        /// <summary>
+        /// 新しいインスタンスを生成します。
+        /// </summary>
+        /// <param name="execute">コマンド実行処理を指定します。</param>
         public DelegateCommand(Action<object> execute)
             : this(execute, null)
         {
         }
-        //新しいインスタンスを生成（引数：execute→コマンド実行処理を指定、canExecute→コマンド実行可能判別処理を指定　のコンストラクタ）
+        /// <summary>
+        /// 新しいインスタンスを生成します。
+        /// </summary>
+        /// <param name="execute">コマンド実行処理を指定します。</param>
+        /// <param name="canExecute">コマンド実行可能判別処理を指定します。</param>
         public DelegateCommand(Action<object> execute, Func<object, bool> canExecute)
         {
             this._execute = execute;
@@ -28,21 +38,33 @@ namespace AIWpfIntroduction.Example
         }
 
         #region ICommand の実装
-        //コマンドが実行可能かどうかの判別処理
+        /// <summary>
+        /// コマンドが実行可能かどうかの判別処理をします。
+        /// </summary>
+        /// <param name="parameter">判別処理に対するパラメータを指定します。</param>
+        /// <returns>実行可能であれば、true を返します。</returns>
         public bool CanExecute(object? parameter)
         {
             return (this._canExecute != null) ? this._canExecute(parameter) : true;
         }
-        //実行可能かどうかの判別処理の関する状態が変更された時に発生
+        /// <summary>
+        /// 実行可能かどうかの判別処理の関する状態が変更された時に発生します。
+        /// </summary>
         public event EventHandler? CanExecuteChanged;
 
+        /// <summary>
+        /// CanExecuteChanged イベントを発行するメソッドです。
+        /// </summary>
         public void RaiseCanExecuteChanged()
         {
             var h = this.CanExecuteChanged;
             if (h != null) h(this, EventArgs.Empty);
         }
 
-        //コマンドが実行されたときの処理
+        /// <summary>
+        /// コマンドが実行されたときの処理です。
+        /// </summary>
+        /// <param name="parameter">コマンドに対するパラメータを指定します。</param>
         public void Execute(object? parameter)
         {
             if(this._execute != null)
