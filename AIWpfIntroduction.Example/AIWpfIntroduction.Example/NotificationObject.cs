42ea25619abb545143533cf5bf92f01b17d46339
Akira Izumi <a-izumi@shibuya.co.jp>
2024-04-03 09:18:13 +0900

    NotificationObjectクラスのコメント追記、usingの整理

--- AIWpfIntroduction.Example/AIWpfIntroduction.Example/NotificationObject.cs
+++ AIWpfIntroduction.Example/AIWpfIntroduction.Example/NotificationObject.cs
@@ -1,26 +1,36 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.ComponentModel;
+﻿using System.ComponentModel;
 using System.Runtime.CompilerServices;
 
 namespace AIWpfIntroduction.Example
 {
+    /// <summary>
+    /// プロパティ値に変更があったタイミングでPropertyChangedイベントを発行して、変更を通知ためのクラスです。
+    /// </summary>
     internal abstract class NotificationObject : INotifyPropertyChanged
     {
         #region InotifyPropertyChanged の実装
-
+        /// <summary>
+        /// プロパティに変更があった場合に発生するイベントです。
+        /// </summary>
         public event PropertyChangedEventHandler? PropertyChanged;
-
+        /// <summary>
+        /// PropertyChangedイベントを発行します。
+        /// </summary>
+        /// <param name="propertyName">プロパティ値に変更があったプロパティ名を指定します。</param>
         protected void RaisePropertyChanged([CallerMemberName]string? propertyName = null)
         {
             var h = this.PropertyChanged;
             if (h != null) h(this, new PropertyChangedEventArgs(propertyName));
         }
 
+        /// <summary>
+        /// プロパティを設定するためのメソッドです。
+        /// </summary>
+        /// <typeparam name="T">プロパティの型を表すジェネリック型パラメータです。（型によって限定しないため）</typeparam>
+        /// <param name="target">プロパティの現在の値を参照するための参照型パラメータです。</param>
+        /// <param name="value">新しい値を指定するためのパラメータです。</param>
+        /// <param name="propertyName">呼び出し元のプロパティ名を表します。</param>
+        /// <returns>プロパティ値に変更があったときに true を返します。</returns>
         protected bool SetProperty<T>(ref T target, T value, [CallerMemberName]string? propertyName = null)
         {
             if (Equals(target, value))
